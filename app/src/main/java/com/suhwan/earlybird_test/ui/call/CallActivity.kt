abc8118ddb8159296793e2653312ba7351a18a3e
umsuhwann <umsuhwann132@gmail.com>
2025-05-23 21:34:19 +0900

    예약한 시간에 전화 기능까지 구현(아직 잠금을 풀지 않고 실행이 안된다)

--- app/src/main/java/com/suhwan/earlybird_test/ui/call/CallActivity.kt
+++ app/src/main/java/com/suhwan/earlybird_test/ui/call/CallActivity.kt
@@ -1,10 +1,13 @@
 package com.suhwan.earlybird_test.ui.call
 
+import android.app.KeyguardManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +21,21 @@ class CallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(KeyguardManager::class.java)
+            keyguardManager.requestDismissKeyguard(this, null)
+        }
+        else{
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+
         binding = ActivityCallBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
